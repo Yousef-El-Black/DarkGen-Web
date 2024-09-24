54584d15af457fdfdda5f3e0fadb5bb797863cf4
Lone Eagle <yousefelblackdev@gmail.com>
2024-09-24 15:41:02 +0300

    Create Preloader

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,15 +1,27 @@
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import Preloader from "../Preloader/Preloader";
 
 const Layout = () => {
-  return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
-  );
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <Header />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  } else {
+    return <Preloader />;
+  }
 };
 
 export default Layout;
